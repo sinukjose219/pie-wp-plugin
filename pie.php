c781f76927bf203c1e0fa81bdc82425c8d0b2ca5
Sinu Jose <sinukjose@gmail.com>
2024-08-30 17:15:06 +0100

    feat: Initalize Project for Pie plugin

---
+++ pie.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Pie
+Plugin URI: https://github.com/sinukjose219/pie-wp-plugin.git
+Description: Wordpress Plugin for Custom Post Type Pie
+Version: 1.0.0
+Author: Sinu Jose
+Author URI: https://linked-in.com/in/sinu-jose/
+*/
